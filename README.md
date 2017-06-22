c4929abc5e4f60210665cd786366a3b46ea383ce
Ricardo-Ke <514068102fk@gmail.com>
2017-06-22 23:00:31 +0800

    FEATURE: add generate twoValueImg function

---
+++ README.md
@@ -0,0 +1,13 @@
+# 大作业 (曾老师) BMP 图像文件解析
+
+## Prerequisite
+
+* node.js 版本不要太低
+
+## Usage
+```
+node index.js path/to/file.bmp path/to/new_file   
+//启动服务 指定源bmp文件的路径(默认顶层目录的pcr.bmp文件)  指定生成的文件(默认在当前目录下生成'twoValueImg.bmp')
+```
+
+然后打开 src/index.html ，bingo~~~
--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
+var express = require('express');
 
+var app = express();
+var bmpFilePath = process.argv[2];
 var encodingFmt = 'ascii';
 
 const fileHeader = {}
 const structHeader = {}
 var fileBuf;
+var step = 0;
 var brightnessData = [];
 
-readFile(path.resolve(__dirname, './pcrbmp.bmp'));
+readFile(path.resolve(__dirname, bmpFilePath || './pcr.bmp'));
 digestHeader(fileBuf.slice(0, 54));   //  resolve header 
 
 // 初始化灰度图统计数据
@@ -18,32 +22,34 @@ for(let i = 0;i < 255;i ++) {
 }
 
 // 判断是否由完整地rgb格式组成
-if(structHeader.biSizeImage % 3 === 0) {
+if(structHeader.biSizeImage % step === 0) {
   getBrightnessData();
+  generateTwoValueBMP();
 }
 
-
-for(let i = 0;i < 255;i ++) {
-  console.log(brightnessData[i]);
-}
-
-// console.log(fileHeader);
-// console.log(structHeader);
-
-function getBrightnessData() {
-  for(let i = fileHeader.bfOffBits;i < structHeader.biSizeImage; i += 3) {
-    let tmp = 0.3 * fileBuf[i] + 0.59 * fileBuf[i + 1] + 0.11 * fileBuf[i + 2]
-      , bindex = Math.ceil(tmp) > 255 ? 255 : Math.ceil(tmp);
-
-    brightnessData[bindex]++;
+// 转换格式，提供给接口
+const resBrightnessData = brightnessData.map(function(val, ind) {
+  return {
+    x: ind,
+    y: val
   }
-  console.log(brightnessData);
-}
+})
 
+// 启动服务
+app.get('/', function(req, res) {
+  var method = req.query.callback
+    , textResBriData = JSON.stringify(resBrightnessData);
+  res.send(200, method + `(${textResBriData})`);
+}).listen(8001);
+
+
+// -------------------------------------
+// 同步读取文件
 function readFile(fileName) {
   fileBuf = fs.readFileSync(fileName);
 }
 
+// 提取头部文件
 function digestHeader(fhBytes) {
   fileHeader.bfType = fhBytes.toString(encodingFmt, 0, 2);
   fileHeader.bfSize = fhBytes.readInt32LE(2);
@@ -62,20 +68,35 @@ function digestHeader(fhBytes) {
   structHeader.biYPelsPerMeter = fhBytes.readInt32LE(42);
   structHeader.biClrUsed = fhBytes.readInt32LE(46);
   structHeader.biClrImportant = fhBytes.readInt32LE(50);
+
+  step = structHeader.biBitCount / 8;
 }
 
-// readFile(path.resolve(__dirname, './SimpleBmpResolver/zyp/res/test.bmp'));
+// 统计灰度值
+function getBrightnessData() {
+  for(let i = fileHeader.bfOffBits;i < structHeader.biSizeImage; i += 3) {
+    let tmp = 0.3 * fileBuf[i] + 0.59 * fileBuf[i + 1] + 0.11 * fileBuf[i + 2]
+      , bindex = Math.ceil(tmp) > 255 ? 255 : Math.ceil(tmp);
 
-// rs.on('data', function(chunk) {
-//   chunks.push(chunk);
-//   size += chunk.length;
-// })
+    brightnessData[bindex]++;
+  }
+}
 
-// rs.on('end', function() {
-//   var buf = new Buffer(size);
-//   buf = Buffer.concat(chunks);
-//   console.log(buf.slice(0, 15).toString('ascii'));
-// })
+// 生成二值变换图
+function generateTwoValueBMP() {
+  const targetUrl = path.resolve(__dirname, process.argv[3] || './twoValueImg.bmp');
 
+  let targetFileBuf = fileBuf.slice();
 
+  for(let i = fileHeader.bfOffBits;i < structHeader.biSizeImage;i += 3) {
+    let result = 0.3*targetFileBuf[i] + 0.59*targetFileBuf[i + 1] + 0.11*targetFileBuf[i + 2]
+      , k = Math.ceil(result) > 128 ? 255 : 0;
+      
+    targetFileBuf[i] = targetFileBuf[i + 1] = targetFileBuf[i + 2] = k;
+  }
 
+  fs.writeFile(targetUrl, targetFileBuf, 'utf-8', function(err) {
+    if (err) throw err;
+    console.log('File has been saved');
+  })
+}
